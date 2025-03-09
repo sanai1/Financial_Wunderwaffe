431482fb85ae27ae91ace771b4bb8f6367aea192
aleksandr <sanek.v.07@gmail.com>
2025-03-09 18:19:34 +0300

    обновлен базовый URL

--- app/src/main/java/com/example/financialwunderwaffe/retrofit/BaseURL.kt
+++ app/src/main/java/com/example/financialwunderwaffe/retrofit/BaseURL.kt
@@ -5,7 +5,7 @@ object BaseURL {
     private var BASE_URL: String? = null
 
     init {
-        BASE_URL = "http://62.113.42.129:8080/api/v1/"
+        BASE_URL = "http://37.252.20.109:8080/api/v1/"
     }
 
     fun getURL(): String = BASE_URL!!
